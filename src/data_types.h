e66e7fbeddc7d03d5a2bee887a782ee7a4318a0d
wronghorsebatterystaple <yhugdrive@gmail.com>
2025-01-31 20:36:07 -0800

    formatting

--- src/data_types.h
+++ src/data_types.h
@@ -4,11 +4,11 @@
 #include <tuple>
 #include <unordered_map>
 
-typedef int Id;
-typedef int Kw;
-typedef std::tuple<Id, Id> IdRange;
-typedef std::tuple<Kw, Kw> KwRange;
-typedef std::tuple<std::string, std::string> QueryToken;
-typedef std::unordered_map<Id, Kw> Db;
+typedef int                                          Id;
+typedef int                                          Kw;
+typedef std::tuple<Id, Id>                           IdRange;
+typedef std::tuple<Kw, Kw>                           KwRange;
+typedef std::tuple<std::string, std::string>         QueryToken;
+typedef std::unordered_map<Id, Kw>                   Db;
 typedef std::unordered_map<KwRange, std::vector<Id>> Index;
 typedef std::unordered_map<std::string, std::string> EncIndex;
