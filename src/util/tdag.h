a94fd96515e25d4a030f89bd6f16256d5498b3ee
AnonymousRand <yhugdrive@gmail.com>
2025-06-02 20:50:24 -0700

    forgot what `friend` meant

--- src/util/tdag.h
+++ src/util/tdag.h
@@ -24,6 +24,10 @@ class TdagNode {
          */
         TdagNode(TdagNode<T>* left, TdagNode<T>* right);
 
+        /**
+         * Traverse subtree of `this` and return all traversed nodes in preorder.
+         */
+        std::list<TdagNode<T>*> traverse();
         std::list<TdagNode<T>*> traverseHelper(std::unordered_set<TdagNode<T>*>& extraParents);
 
     public:
@@ -34,11 +38,6 @@ class TdagNode {
         TdagNode(T maxLeafVal);
         
         ~TdagNode();
-
-        /**
-         * Traverse subtree of `this` and return all traversed nodes in preorder.
-         */
-        std::list<TdagNode<T>*> traverse();
 
         /**
          * Find the single range cover of the leaves containing `range`.
