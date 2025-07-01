067504b604bcf61841f000e2980278f91d15327c
AnonymousRand <yhugdrive@gmail.com>
2025-07-01 07:32:02 +0000

    wip

--- src/pi_bas.cpp
+++ src/pi_bas.cpp
@@ -14,15 +14,6 @@ PiBasBase<DbDoc, DbKw>::PiBasBase(EncIndType encIndType) {
 
 
 template <IDbDoc_ DbDoc, class DbKw>
-PiBasBase<DbDoc, DbKw>::~PiBasBase() {
-    if (this->encInd != nullptr) {
-        delete this->encInd;
-        this->encInd = nullptr;
-    }
-}
-
-
-template <IDbDoc_ DbDoc, class DbKw>
 std::vector<DbDoc> PiBasBase<DbDoc, DbKw>::search(
     const Range<DbKw>& query, bool shouldCleanUpResults, bool isNaive
 ) const {
@@ -49,9 +40,6 @@ std::vector<DbDoc> PiBasBase<DbDoc, DbKw>::search(
 
 template <IDbDoc_ DbDoc, class DbKw>
 void PiBasBase<DbDoc, DbKw>::clear() {
-    if (this->encInd != nullptr) {
-        this->encInd->clear();
-    }
     this->db.clear();
     this->_isEmpty = true;
 }
@@ -69,19 +57,6 @@ bool PiBasBase<DbDoc, DbKw>::isEmpty() const {
 }
 
 
-template <IDbDoc_ DbDoc, class DbKw>
-void PiBasBase<DbDoc, DbKw>::setEncIndType(EncIndType encIndType) {
-    switch (encIndType) {
-        case EncIndType::RAM:
-            this->encInd = new EncIndRam();
-            break;
-        case EncIndType::DISK:
-            this->encInd = new EncIndDisk();
-            break;
-    }
-}
-
-
 /******************************************************************************/
 /* `PiBas`                                                                    */
 /******************************************************************************/
@@ -92,13 +67,19 @@ PiBas<DbDoc, DbKw>::PiBas(EncIndType encIndType) : PiBasBase<DbDoc, DbKw>(encInd
 
 
 template <IDbDoc_ DbDoc, class DbKw>
+PiBas<DbDoc, DbKw>::~PiBas() {
+    this->clear();
+}
+
+
+template <IDbDoc_ DbDoc, class DbKw>
 void PiBas<DbDoc, DbKw>::setup(int secParam, const Db<DbDoc, DbKw>& db) {
     this->clear();
 
     ////////////////////////////// generate keys ///////////////////////////////
 
-    this->secParam = secParam;
-    this->key = genKey(this->secParam);
+    this->keyPrf = genKey(secParam);
+    this->keyEnc = genKey(secParam);
 
     /////////////////////////////// build index ////////////////////////////////
 
@@ -127,11 +108,8 @@ void PiBas<DbDoc, DbKw>::setup(int secParam, const Db<DbDoc, DbKw>& db) {
     std::unordered_set<Range<DbKw>> uniqDbKwRanges = getUniqDbKwRanges(db);
     // for each w in W
     for (Range<DbKw> dbKwRange : uniqDbKwRanges) {
-        // K_1 || K_2 <- F(K, w)
-        // the paper uses different notation for the key generation here vs. in `genQueryToken()`
-        // (`Trpdr`), but I'm fairly sure they mean the same thing, otherwise it doesn't work
-        std::pair<ustring, ustring> subkeys = this->genQueryToken(dbKwRange);
-        
+        ustring prfOutput = this->genQueryToken(dbKwRange);
+        // TODO finish pseudocode comments
         // for each id in DB(w)
         auto iter = ind.find(dbKwRange);
         if (iter == ind.end()) {
@@ -140,12 +118,10 @@ void PiBas<DbDoc, DbKw>::setup(int secParam, const Db<DbDoc, DbKw>& db) {
         std::vector<DbDoc> dbDocsWithSameKw = iter->second;
         for (long counter = 0; counter < dbDocsWithSameKw.size(); counter++) {
             DbDoc dbDoc = dbDocsWithSameKw[counter];
-            // l <- F(K_1, c); d <- Enc(K_2, id); c++
-            ustring label = prf(subkeys.first, toUstr(counter));
+            ustring label = findHash(HASH_FUNC, HASH_OUTPUT_LEN, prfOutput + toUstr(counter));
             ustring iv = genIv(IV_LEN);
             // for some reason padding to exactly n blocks generates n + 1 blocks, so we pad to one less byte
-            ustring encryptedDbDoc = padAndEncrypt(ENC_CIPHER, subkeys.second, dbDoc.toUstr(), iv, ENC_IND_DOC_LEN - 1);
-            // add (l, d) to list L (in lex order); we don't need to sort ourselves since C++ has ordered maps
+            ustring encryptedDbDoc = padAndEncrypt(ENC_CIPHER, this->keyEnc, dbDoc.toUstr(), iv, ENC_IND_DOC_LEN - 1);
             // also store IV in plain along with encrypted value
             this->encInd->write(label, std::pair {encryptedDbDoc, iv});
         }
@@ -156,113 +132,9 @@ void PiBas<DbDoc, DbKw>::setup(int secParam, const Db<DbDoc, DbKw>& db) {
 template <IDbDoc_ DbDoc, class DbKw>
 std::vector<DbDoc> PiBas<DbDoc, DbKw>::searchBase(const Range<DbKw>& query) const {
     std::vector<DbDoc> results;
-    std::pair<ustring, ustring> queryToken = this->genQueryToken(query);
-    ustring subkeyPrf = queryToken.first;
-    ustring subkeyEnc = queryToken.second;
-    
-    // for c = 0 until `Get` returns error
-    long counter = 0;
-    while (true) {
-        // d <- Get(D, F(K_1, c)); c++
-        ustring label = prf(subkeyPrf, toUstr(counter));
-        std::pair<ustring, ustring> encIndVal;
-        int status = this->encInd->find(label, encIndVal);
-        if (status == -1) {
-            break;
-        }
-        ustring encryptedDbDoc = encIndVal.first;
-        // id <- Dec(K_2, d)
-        ustring iv = encIndVal.second;
-        ustring decryptedDbDoc = decryptAndUnpad(ENC_CIPHER, subkeyEnc, encryptedDbDoc, iv);
-        DbDoc result = DbDoc::fromUstr(decryptedDbDoc);
-        results.push_back(result);
-        counter++;
-    }
-
-    return results;
-}
-
-
-template <IDbDoc_ DbDoc, class DbKw>
-std::pair<ustring, ustring> PiBas<DbDoc, DbKw>::genQueryToken(const Range<DbKw>& query) const {
-    ustring K = prf(this->key, query.toUstr());
-    int subkeyLen = K.length() / 2;
-    ustring subkeyPrf = K.substr(0, subkeyLen);
-    ustring subkeyEnc = K.substr(subkeyLen, subkeyLen);
-    return std::pair {subkeyPrf, subkeyEnc};
-}
-
-
-template class PiBas<Doc, Kw>;        // PiBas
-template class PiBas<SrcIDb1Doc, Kw>; // Log-SRC-i index 1
-//template class PiBas<Doc, IdAlias>; // Log-SRC-i index 2
-
-
-/******************************************************************************/
-/* `PiBasResHiding`                                                           */
-/******************************************************************************/
-
-
-template <IDbDoc_ DbDoc, class DbKw>
-PiBasResHiding<DbDoc, DbKw>::PiBasResHiding(EncIndType encIndType) : PiBasBase<DbDoc, DbKw>(encIndType) {}
-
-
-template <IDbDoc_ DbDoc, class DbKw>
-void PiBasResHiding<DbDoc, DbKw>::setup(int secParam, const Db<DbDoc, DbKw>& db) {
-    this->clear();
-
-    ////////////////////////////// generate keys ///////////////////////////////
-
-    this->keyPrf = genKey(secParam);
-    this->keyEnc = genKey(secParam);
-
-    /////////////////////////////// build index ////////////////////////////////
-
-    this->db = db;
-    this->_isEmpty = db.empty();
-    if (db.empty()) {
-        return;
-    }
-
-    // generate (plaintext) index of keywords to documents/ids mapping and list of unique keywords
-    Ind<DbKw, DbDoc> ind;
-    for (DbEntry<DbDoc, DbKw> entry : db) {
-        DbDoc dbDoc = entry.first;
-        Range<DbKw> dbKwRange = entry.second;
-
-        if (ind.count(dbKwRange) == 0) {
-            ind[dbKwRange] = std::vector {dbDoc};
-        } else {
-            ind[dbKwRange].push_back(dbDoc);
-        }
-    }
-
-    this->encInd->init(db.size());
-    std::unordered_set<Range<DbKw>> uniqDbKwRanges = getUniqDbKwRanges(db);
-    // for each w in W
-    for (Range<DbKw> dbKwRange : uniqDbKwRanges) {
-        ustring prfOutput = this->genQueryToken(dbKwRange);
-        auto iter = ind.find(dbKwRange);
-        if (iter == ind.end()) {
-            continue;
-        }
-        std::vector<DbDoc> dbDocsWithSameKw = iter->second;
-        for (long counter = 0; counter < dbDocsWithSameKw.size(); counter++) {
-            DbDoc dbDoc = dbDocsWithSameKw[counter];
-            ustring label = findHash(HASH_FUNC, HASH_OUTPUT_LEN, prfOutput + toUstr(counter));
-            ustring iv = genIv(IV_LEN);
-            ustring encryptedDbDoc = padAndEncrypt(ENC_CIPHER, this->keyEnc, dbDoc.toUstr(), iv, ENC_IND_DOC_LEN - 1);
-            this->encInd->write(label, std::pair {encryptedDbDoc, iv});
-        }
-    }
-}
-
-
-template <IDbDoc_ DbDoc, class DbKw>
-std::vector<DbDoc> PiBasResHiding<DbDoc, DbKw>::searchBase(const Range<DbKw>& query) const {
-    std::vector<DbDoc> results;
     ustring queryToken = this->genQueryToken(query);
         
+    // for c = 0 until `Get` returns error
     long counter = 0;
     while (true) {
         ustring label = findHash(HASH_FUNC, HASH_OUTPUT_LEN, queryToken + toUstr(counter));
@@ -286,11 +158,33 @@ std::vector<DbDoc> PiBasResHiding<DbDoc, DbKw>::searchBase(const Range<DbKw>& qu
 
 
 template <IDbDoc_ DbDoc, class DbKw>
-ustring PiBasResHiding<DbDoc, DbKw>::genQueryToken(const Range<DbKw>& query) const {
+ustring PiBas<DbDoc, DbKw>::genQueryToken(const Range<DbKw>& query) const {
     return prf(this->keyPrf, query.toUstr());
 }
 
 
-template class PiBasResHiding<Doc, Kw>;       
-template class PiBasResHiding<SrcIDb1Doc, Kw>;
-//template class PiBasResHiding<Doc, IdAlias>;
+template <IDbDoc_ DbDoc, class DbKw>
+void PiBas<DbDoc, DbKw>::clear() {
+    PiBasBase<DbDoc, DbKw>::clear();
+    if (this->encInd != nullptr) {
+        this->encInd->clear();
+    }
+}
+
+
+template <IDbDoc_ DbDoc, class DbKw>
+void PiBas<DbDoc, DbKw>::setEncIndType(EncIndType encIndType) {
+    switch (encIndType) {
+        case EncIndType::RAM:
+            this->encInd = new EncIndRam();
+            break;
+        case EncIndType::DISK:
+            this->encInd = new EncIndDisk();
+            break;
+    }
+}
+
+
+template class PiBas<Doc, Kw>;        // PiBas
+template class PiBas<SrcIDb1Doc, Kw>; // Log-SRC-i index 1
+//template class PiBas<Doc, IdAlias>; // Log-SRC-i index 2
--- src/pi_bas.h
+++ src/pi_bas.h
@@ -9,20 +9,23 @@
 /******************************************************************************/
 
 
-// common code between `PiBas` and `PiBasResHiding`
+// common code between `PiBas` and `PiBasLoc`
+// note that we use the result-hiding variant of PiBas since SDa wants that iirc
 template <IDbDoc_ DbDoc, class DbKw>
 class PiBasBase : public ISse<DbDoc, DbKw>, public ISdaUnderly<DbDoc, DbKw> {
     protected:
+        ustring keyPrf;
+        ustring keyEnc;
         Db<DbDoc, DbKw> db;
-        IEncInd* encInd = nullptr;
         bool _isEmpty = false;
 
         virtual std::vector<DbDoc> searchBase(const Range<DbKw>& query) const = 0;
+        ustring genQueryToken(const Range<DbKw>& query) const;
 
     public:
         PiBasBase() = default;
         PiBasBase(EncIndType encIndType);
-        virtual ~PiBasBase();
+        virtual ~PiBasBase() = default;
 
         std::vector<DbDoc> search(
             const Range<DbKw>& query, bool shouldCleanUpResults = true, bool isNaive = true
@@ -31,43 +34,52 @@ class PiBasBase : public ISse<DbDoc, DbKw>, public ISdaUnderly<DbDoc, DbKw> {
         void clear() override;
         Db<DbDoc, DbKw> getDb() const override;
         bool isEmpty() const override;
-        void setEncIndType(EncIndType encIndType) override;
 };
+
+
+/******************************************************************************/
+/* `PiBas`                                                                    */
+/******************************************************************************/
 
 
 template <IDbDoc_ DbDoc = Doc, class DbKw = Kw>
 class PiBas : public PiBasBase<DbDoc, DbKw> {
     private:
-        ustring key;
+        IEncInd* encInd = nullptr;
 
         std::vector<DbDoc> searchBase(const Range<DbKw>& query) const override;
-        std::pair<ustring, ustring> genQueryToken(const Range<DbKw>& query) const;
 
     public:
         PiBas() = default;
         PiBas(EncIndType encIndType);
+        ~PiBas();
 
         void setup(int secParam, const Db<DbDoc, DbKw>& db) override;
+
+        void clear() override;
+        void setEncIndType(EncIndType encIndType) override;
 };
 
 
 /******************************************************************************/
-/* `PiBasResHiding`                                                           */
+/* `PiBasLoc`                                                                 */
 /******************************************************************************/
 
 
 template <IDbDoc_ DbDoc = Doc, class DbKw = Kw>
-class PiBasResHiding : public PiBasBase<DbDoc, DbKw> {
+class PiBasLoc : public PiBasBase<DbDoc, DbKw> {
     private:
-        ustring keyPrf;
-        ustring keyEnc;
+        IEncIndLoc* encInd = nullptr;
 
         std::vector<DbDoc> searchBase(const Range<DbKw>& query) const override;
-        ustring genQueryToken(const Range<DbKw>& query) const;
 
     public:
-        PiBasResHiding() = default;
-        PiBasResHiding(EncIndType encIndType);
+        PiBasLoc() = default;
+        PiBasLoc(EncIndType encIndType);
+        ~PiBasLoc();
 
         void setup(int secParam, const Db<DbDoc, DbKw>& db) override;
+
+        void clear() override;
+        void setEncIndType(EncIndType encIndType) override;
 };
