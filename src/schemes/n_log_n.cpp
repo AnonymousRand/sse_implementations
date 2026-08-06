7494068defe2506bbe16f657da8121d8c7535cfb
AnonymousRand <anonymouserand@gmail.com>
2026-08-06 00:14:53 -0700

    i think i fixed it :3

--- src/schemes/n_log_n.cpp
+++ src/schemes/n_log_n.cpp
@@ -107,7 +107,7 @@ void NLogN<DbDoc, DbKw>::setup(int secParam, const Db<DbDoc, DbKw>& db) {
             ustring iv = genIv(IV_LEN);
             ustring encDbDoc = padAndEncrypt(ENC_CIPHER, this->encKey, dbDoc.toUstr(), iv, EncInd::DOC_LEN - 1);
             // store `(l, d)` into key-value store, and also store IV in plain along with `d`
-            this->server->writeToEncInd(startPos + dbKwCounter, lvl, std::pair {label, std::pair {encDbDoc, iv}});
+            this->server->writeToEncInd(lvl, startPos + dbKwCounter, std::pair {label, std::pair {encDbDoc, iv}});
         }
     }
 }
