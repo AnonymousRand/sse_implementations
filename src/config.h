4be4423fd303f0f4b5f221996638efd99fce929e
AnonymousRand <anonymouserand@gmail.com>
2026-08-10 15:25:59 -0700

    turn off update benchmarking in config for noow

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@ namespace config {
 
 inline constexpr bool SHOULD_BENCHMARK = true;
 inline constexpr bool DSSE_USE_SHORTCUT_SETUP = true;
-inline constexpr bool DSSE_SHOULD_BENCHMARK_UPDTS = true;
+inline constexpr bool DSSE_SHOULD_BENCHMARK_UPDTS = false;
 
 
 } // namespace `config`
