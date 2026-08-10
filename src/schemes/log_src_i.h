98ca3ec6565dcc3d6b96691e9c5e9d84437151a3
AnonymousRand <anonymouserand@gmail.com>
2026-08-09 18:42:57 -0700

    minor formatting

--- src/schemes/log_src_i.h
+++ src/schemes/log_src_i.h
@@ -36,14 +36,14 @@ public:
 protected:
     Underly<SrcIDb1Doc, Kw>* underly1 = new Underly<SrcIDb1Doc, Kw>(this->benchmark);
     Underly<Doc<IdAlias>, IdAlias>* underly2 = new Underly<Doc<IdAlias>, IdAlias>(this->benchmark);
+    TdagNode<Kw>* tdag1 = nullptr;
+    TdagNode<IdAlias>* tdag2 = nullptr;
     // this is only used to store the original db in `getDb()` so that it is encrypted but easy to recover
     // instead of reconstructing the original db from `underly1`'s and `underly2`'s indexes/dbs
     // do NOT search on this one!
     // also it's specifically PiBas since `LogSrcIStar` may use locality-aware `Underly`,
     // which doesn't store non-TDAG-structured datasets correctly or efficiently (e.g. they might pad)
     PiBas<Doc<>, Kw>* origDbUnderly = new PiBas<Doc<>, Kw>(this->benchmark);
-    TdagNode<Kw>* tdag1 = nullptr;
-    TdagNode<IdAlias>* tdag2 = nullptr;
 };
 
 
