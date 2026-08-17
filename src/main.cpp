1233f62238422d0781f9eded9f2a0487ac5c2b62
AnonymousRand <anonymouserand@gmail.com>
2026-08-17 11:02:05 -0700

    oh my gosh i'm so bad

--- src/main.cpp
+++ src/main.cpp
@@ -297,11 +297,11 @@ int main() {
     std::cout << "========= SDa[Log-SRC[PiBas]] ==========" << std::endl << std::endl;
     searchVsFalsePos.run(sdaLogSrcPiBas.get(), config::SHOULD_BENCHMARK);
 
-    std::cout << "========= SDa[Log-SRC[NLogN]] ==========" << std::endl << std::endl;
-    searchVsFalsePos.run(sdaLogSrcNLogN.get(), config::SHOULD_BENCHMARK);
-
     std::cout << "======== SDa[Log-SRC-i[PiBas]] =========" << std::endl << std::endl;
     searchVsFalsePos.run(sdaLogSrcIPiBas.get(), config::SHOULD_BENCHMARK);
+
+    std::cout << "========= SDa[Log-SRC[NLogN]] ==========" << std::endl << std::endl;
+    searchVsFalsePos.run(sdaLogSrcNLogN.get(), config::SHOULD_BENCHMARK);
 
     std::cout << "======== SDa[Log-SRC-i[NLogN]] =========" << std::endl << std::endl;
     searchVsFalsePos.run(sdaLogSrcINLogN.get(), config::SHOULD_BENCHMARK);
