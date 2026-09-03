d2e23c0a2876e0a32ad622d979a79b01e46d0368
AnonymousRand <--global>
2026-09-03 18:49:56 +0800

    tweak

--- src/utils/types/tuple.cpp
+++ src/utils/types/tuple.cpp
@@ -104,7 +104,7 @@ Tuple<DbKw> Tuple<DbKw>::fromStr(const std::string& str) {
     std::smatch matches;
     if (!std::regex_search(str, matches, REGEX) || matches.size() != 5) {
         std::cerr << "Error: Tuple::fromStr(): bad string \"" << str << "\" passed" << std::endl
-                  << "Regex to match is \"" << REGEX_STR << "\"; " << "matched groups are:"
+                  << "Regex to match is \"" << REGEX_STR << "\"; matched groups are:"
                   << std::endl;
         for (auto match : matches) {
             std::cerr << match.str() << std::endl;
