7e45430409d26a3f48cafa6477ea03603c8b95a2
AnonymousRand <anonymouserand@gmail.com>
2026-09-23 18:47:23 -0700

    overlooked srcidb1tuple encodings

--- src/config.h
+++ src/config.h
@@ -92,20 +92,21 @@ inline constexpr int ENCODED_NUMBER_BITS = 29;
  * the max number of decimal digits you want ids and keywords to be able to support
  * (this determines the size of each entry in encrypted indexes; see `TUPLE_ENCOD_LEN` below).
  */
-// currently: 11 is the largest possible value such that each encrypted tuple fits in
-// 3 AES blocks (= 48 bytes)
-inline constexpr int MAX_VALUE_DIGITS = 11;
+// currently: 8 is the largest possible value such that each encrypted tuple fits in
+// 3 AES blocks (= 48 bytes), and corresponds to DB sizes up to 2^26
+inline constexpr int MAX_VALUE_DIGITS = 8;
 static_assert(
     MAX_VALUE_DIGITS > 0, "Error: `MAX_VALUE_DIGITS` must be strictly positive!"
 );
 
-// currently, encoding a `Tuple<>` is of the form `id,kw[op]dbKw-dbKw`, so all but 3 bytes are
-// divided up between `id`, `kw`, and 2 `dbKw`s. however, we actually must restrict our plaintexts
-// by one more byte or else AES' PCKS #7 padding will generate an extra block if our plaintext is
-// exactly block-aligned, thus the `+ 4`. also, `SrcIDb1Tuple`s have the same max length encoding.
+// currently, encoding a `Tuple<>` is of the form `id,kw[op]dbKw-dbKw` while an `SrcIDb1Doc` is
+// `kw,id'-id',kw-kw`, meaning the latter is the larger encoding. there, all but 4 bytes are
+// divided up between `kw`, 2 `id'`s, and 2 `kw`s. however, we actually must restrict our
+// plaintexts by one more byte or else AES' PCKS #7 padding will generate an extra block if
+// our plaintext is exactly block-aligned, thus the `+ 5`.
 // IMPORTANT: update if encoding changes!
 inline constexpr int TUPLE_ENCOD_LEN =
-    std::ceil((4 * MAX_VALUE_DIGITS + 4) / (float)utils::crypto::BLOCK_SIZE)
+    std::ceil((5 * MAX_VALUE_DIGITS + 5) / (float)utils::crypto::BLOCK_SIZE)
     * utils::crypto::BLOCK_SIZE;
 
 
